c94d716247bc921e108f94ab8190aa67bb7e9295
Ashley <ashleymarsh106@gmail.com>
2020-05-21 15:07:07 +0100

    updated quiz files

--- quiz.js
+++ quiz.js
@@ -2,122 +2,152 @@
 
 const quiz = [
     {
+        number: "1",
         question: "In early January, which country landed the first human-made object on the far side of the Moon?", //question 1 
         answer: "China"
     },
     {
+        number: "2",
         question: "The NHS banned the purchasing of which piece of equipment - the first experimental version being invented by Scotland's Alexander Bain in 1846?",//question 2 
         answer: "Fax machines"
     },
     {
+        number: "3",
         question: 'Who was unhurt, "but very, very shocked and shaken."?',//question 3
         answer: "The Duke of Edinburgh"
     },
     {
+        number: "4",
         question: "Premier League striker Emiliano Sala's plane went missing while on a flight from Nantes to which British city?",//question 4
         answer: "Cardiff"
     },
     {
+        number: "5",
         question: "Who announced he was leaving The National Television Awards after 9 years as host?",//question 5
         answer: "Dermot O'Leary"
     },
     {
+        number: "6",
         question: "A picture of which everyday object on Instagram became a global phenomenon by becoming the most liked post in online history?",//question 6
         answer: "An egg (known as the Instagram egg)"
     },
     {
+        number: "7",
         question: "Who launched a legal action against the makers of Channel 4's The Jump after fracturing vertebrae in 2016?",//question 7
         answer: "Beth Tweddle"
     },
     {
+        number: "8",
         question: "January saw the sad death of actor Windsor Davies; in which 'Carry On' film did he play Sergeant-Major 'Tiger' Bloomer?",//question 8
         answer: "Carry on England"
     },
     {
+        number: "9",
         question: "Which city's MP became the first sitting MP to be jailed in 28 years?",//question 9
         answer: "Peterborough's"
     },
     {
+        number: "10",
         question: "The U.S. Justice Department charged which Chinese tech firm with multiple counts of fraud?",//question 10
         answer: "Huawei"
     },
     {
+        number: "11",
         question: "On February the 3rd, who arrived in Abu Dhabi in the United Arab Emirates?",//question 11
         answer: "Pope Francis (becoming the first pope to visit the Arabian Peninsula)"
     },
     {
+        number: "12",
         question: "Which European country renamed itself, hence ending a decades-old dispute with Greece?",//question 12
         answer: "The Republic of Macedonia renamed itself the Republic of North Macedonia"
     },
     {
+        number: "13",
         question: "The court-case featuring Joaquin Archivaldo Guzman Loera made world headlines, by what name is this Mexican drug lord better known?",//question 13
         answer: "El Chapo"
     },
     {
+        number: "14",
         question: "Albert Finney sadly passed away. In the classic 1960 film Saturday Night and Sunday Morning which product did his character Arthur Seaton make in a Nottingham factory?",//question 14
         answer: "Raleigh bicycles"
     },
     {
+        number: "15",
         question: "Change UK – The Independent Group was founded, who was its interim leader?",//question 15
         answer: "Heidi Allen"
     },
     {
+        number: "16",
         question: "Who became the oldest football manager in Premier League history, at the age of 71 years and 198 days?",//question 16
         answer: "Roy Hodgson (Crystal Palace)"
     },
     {
+        number: "17",
         question: "Who did BBC Two viewers vote the most iconic figure of the 20th Century following a series that asked the public to name the century's greatest figure?",//question 17
         answer: "Alan Turing"
     },
     {
+        number: "18",
         question: "Who announces his intention to step down from his presenting role on BBC Radio 4's The Today Programme later in the year?",//question 18
         answer: "John Humphrys"
     },
     {
+        number: "19",
         question: "The family of which American fiction writer confirm that he left a large unpublished body of work on his death in 2010 which they are preparing for publication?",//question 19
         answer: "J. D. Salinger"
     },
     {
+        number: "20",
         question: "Chelsea goalkeeper Kepa Arrizabalaga apologised for refusing to do what during the Cup final?",//question 20
         answer: "Leaving the pitch/ refused to be substituted"
     },
     {
+        number: "21",
         question: "The February North Korea–United States summit was held in which country?",//question 21
         answer: "Vietnam"
     },
     {
+        number: "22",
         question: "In early March, Keith Charles Flint sadly passed away, he was a vocalist with which electronic dance act?",//question 22
         answer: "The Prodigy"
     },
     {
+        number: "23",
         question: "Name the Desperate Housewives actress caught up in the American college admissions bribery scandal?",//question 23
         answer: "Felicity Kendall Huffman"
     },
     {
+        number: "24",
         question: "Which denomination coin celebrating Stephen Hawking’s work on black holes was unveiled in March?",//question 24
         answer: "Fifty pence piece"
     },
     {
+        number: "25",
         question: "What did retired banker Irini Tzortzoglou win?",//question 25
         answer: "The 2019 series of MasterChef"
     },
     {
+        number: "26",
         question: "In which bay did the cargo ship Grande America sink causing a 2,200-tonne oil spill?",//question 26
         answer: "Bay of Biscay"
     },
     {
+        number: "27",
         question: "Who made news headlines by seperating from her husband Simon Konecki?",//question 27
         answer: "Adele"
     },
     {
+        number: "28",
         question: "In rugby, which country did Wales beat at the Millennium Stadium on the final day of the Six Nations to claim their first Grand Slam since 2012?",//question 28
         answer: "Ireland"
     },
     {
+        number: "29",
         question: "A plane destined for Dusseldorf unexpectedly arrived in which British city's airport due to incorrect paperwork?",//question 29
         answer: "Edinburgh"
     },
     {
+        number: "30",
         question: "In late March, it was announced that Norway will return thousands of artefacts taken in 1956 by adventurer Thor Heyerdahl from which island?",//question 30
         answer: "Easter Island"
     },
--- scripts.js
+++ scripts.js
@@ -1,17 +1,59 @@
 
+
 let questionCount = 0;
+// let correctAnswer = quiz[questionCount].answer;
+
+//correct - incorrect answer count
+let correctAnswerCount = 0;
+let incorrectAnswer = 0;
+
 const question = document.querySelector('p');
+const inputAnswer = document.getElementById('inputAnswer');
 const submitAnswer = document.getElementById('submitAnswer');
+
+
+// changes questions displayed on page
 question.innerHTML = quiz[0].question;
 
+
+// function called from event
 let questionCountPlus = function() {
+
+    //cycles through correct answer 
+    let correctAnswer = quiz[questionCount].answer;
+
+    //display scores to page ---this needs work.
+    document.querySelector('h3.scores').innerText = `Your score was ${correctAnswerCount}`
+
+
+    if (inputAnswer.value === correctAnswer) {
+        // is correct
+
+        correctAnswerCount += 1;
+        console.log("that was correct");
+
+        //clears the input for next question
+        inputAnswer.value = "";
+
+    } else {
+        //is incorrect
+
+        incorrectAnswer += 1;
+        console.log("that was incorrect");
+
+        //clears the input for next question
+        inputAnswer.value = "";
+    }
+
+    
+
     questionCount += 1;
+    document.querySelector(`div.question-card h3.question`).innerText = `Question ${quiz[questionCount].number}`;
+    document.querySelector(`div.question-card p`).innerText = quiz[questionCount].question;
 }
 
-submitAnswer.addEventListener("click", questionCountPlus);
+// event "click"ing submitAnswer button
+submitAnswer.addEventListener("click", questionCountPlus,);
  
-if (questionCount > 1) {
-    console.log("good");
-}
 
-console.log(questionCount);
+
